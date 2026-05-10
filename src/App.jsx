a5e87e7db63ba14555a89e966d1a892e12059f30
Hariprasath <hariprasath@adhiba.com>
2026-05-10 21:46:11 +0530

    Updated App design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Home from "./pages/home";
+import Support from "./pages/support";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </>
   );
